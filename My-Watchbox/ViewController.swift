e999f5c46adea05b5291d645b05ffa0e42c04863
alam7989 <68211754+alam7989@users.noreply.github.com>
2023-11-15 12:18:56 -0800

    Created basic view controllers for List Feed and Entry Details pages

---
+++ My-Watchbox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My-Watchbox
+//
+//  Created by Audrey Lam on 11/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
