b13f948d4978acba0acc47ba7d5725b5c0ac8363
Audrey Lam <audreylam989@gmail.com>
2023-11-15 17:45:03 -0800

    implemented basic layout of List Feed view controller

---
+++ My-Watchbox/CustomTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  CustomTableViewCell.swift
+//  My-Watchbox
+//
+//  Created by Audrey Lam on 11/15/23.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var entryCell: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
